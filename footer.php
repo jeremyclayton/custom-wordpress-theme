14541a7e6f19a6e33684e07e1be857b9ee0d49a5
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-07-13 09:12:50 -0600

    added existing code base into git repo

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="footer-clear"></div>
+<footer class="row no-max pad">
+    <p><?php echo date ('Y'); ?></p>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ sidebar-page.php
@@ -0,0 +1,8 @@
+<div class="small-12 medium-4 medium-pull-8 columns">
+    <div class="secondary">
+        <?php if(!dynamic_sidebar('page') ) : ?>
+            <h2 class="module-heading">No Widgets Found</h2>
+            <p>please set up widgets in admin area!</p>
+        <?php endif; ?>
+    </div>
+</div>
